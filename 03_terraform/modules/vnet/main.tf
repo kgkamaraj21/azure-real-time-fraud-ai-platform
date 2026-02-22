fef3f085bf7b10074ba1abb0e208e6dbc045e7b8
kgkamaraj21 <kgkamaraj@gmail.com>
2026-02-22 12:50:51 +0530

    VNet Module

---
+++ 03_terraform/modules/vnet/main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = var.rg_name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "data_subnet" {
+  name                 = "data-subnet"
+  resource_group_name  = var.rg_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "app-subnet"
+  resource_group_name  = var.rg_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
